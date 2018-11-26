3c5af5cb90ba41b81937bb70dd0c9dc1c11e9dc4
Mike Osenni <mosenni@fandm.edu>
2018-11-26 02:17:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function johnLennonFacts(){
 var facts = ["He was the last Beatle to learn to drive",
 "He was never a vegetarian",
  "He was a choir boy and boy scout",
-"He hates the sound of his own voice"];
+"He hated the sound of his own voice"];
 
 var truths = [];
 let i = 0
