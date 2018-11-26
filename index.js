cc3667c3137db6fcac090af88b28997f4682d16d
Mike Osenni <mosenni@fandm.edu>
2018-11-26 02:17:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ function theBeatlesPlay(musicians, instruments){
  }
  return theBand
 }
+
+function johnLennonFacts(){
+var facts = ["He was the last Beatle to learn to drive", 
+"He was never a vegetarian",
+ "He was a choir boy and boy scout",
+"He hates the sound of his own voice"];
+
+var truths = [];
+let i = 0
+while (i < facts.length) {
+  truths.push(facts[i] + "!!!");
+  i++;
+}
+  return truths
+}
