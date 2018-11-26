bd3c75e13414de4a6f6eac04894255a39af142b6
Mike Osenni <mosenni@fandm.edu>
2018-11-26 02:13:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 function theBeatlesPlay(musicians, instruments){
  var theBand = [];
  for (let i = 0; i < musicians.length; i++) {
-   theBand.push(players[i] + " plays " + insturments[i]);
+   theBand.push(musicians[i] + " plays " + insturments[i]);
 
  }
  return theBand
